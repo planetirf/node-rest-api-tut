341935bd415092973877ea74737cfb97c2173943
Irfan Ainuddin <irfandin@gmail.com>
2020-03-30 22:08:23 -0700

    order.js updated

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -9,6 +9,7 @@ const Product = require('../models/product')
 router.get('/', (req, res, next) => {
     Order.find()
         .select('-__v')
+        .populate('product','name')
         .exec()
         .then(docs => {
             res.status(200).json({
@@ -75,6 +76,7 @@ router.post('/', (req, res, next) => {
 
 router.get('/:orderId', (req, res, next) => {
     Order.findById(req.params.orderId)
+        .populate('product','name')
         .exec()
         .then(order => {
             if(!order) {
